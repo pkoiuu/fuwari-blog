43c815bd7478df0a4f35af46e59665f3d1023d4c
黄浩景 <SY111125@outlook.com>
2026-02-04 14:21:14 +0800

    ghgfhgf

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -1,3 +1,4 @@
-export function formatDateToYYYYMMDD(date: Date): string {
+export function formatDateToYYYYMMDD(date: Date | undefined | null): string {
+	if (!date) return "";
 	return date.toISOString().substring(0, 10);
 }
